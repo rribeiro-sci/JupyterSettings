4020b8fd5a40fe1168c0cbdd965510e511f8fb6d
Rui Ribeiro <rui.ribeiro@icloud.com>
2024-02-21 11:21:38 +0100

    first commit

---
+++ JupyterSettings.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024 Rui Ribeiro
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+A Python library of functions for modifying Jupyter settings.
+"""
+__author__ = "Rui Ribeiro"
+__email__ = "rui.ribeiro@univr.it"
+
+
+
+def AutoReload():
+    """
+    Enabling automatic reloading of modules before executing each line of code.
+    """
+    from IPython import get_ipython
+
+    # Load the autoreload extension
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+
+    # Set autoreload behavior
+    get_ipython().run_line_magic('autoreload', '2')
+
+def CellSize(size):
+    """
+    Defining the cell size based on percentage of space that cell will occupy.
+
+    :parameter size: (int) percentage (eg: 98)
+    """
+    from IPython.display import display, HTML
+    display(HTML(f'<style>.container {{width: {size}% !important;}}<\style>'))
+
+def Retina():
+    """
+    Rendering matplotlib plots in high resolution.
+    """
+    import matplotlib_inline
+    matplotlib_inline.backend_inline.set_matplotlib_formats('retina')
+
+def SideBySide(bool):
+    """
+    Displaying widgets horizontally when True
+    """
+    from IPython.display import display, HTML
+    if bool==True: display(HTML("<style>.output {flex-direction: row;}</style>"))
+    else: display(HTML("<style>.output {flex-direction: column;}</style>"))
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# JupyterSettings
+
+JupyterSettings is a Python library designed to simplify the management and customization of Jupyter Notebook settings. It provides a range of functionalities for configuring various aspects of Jupyter Notebooks to suit individual preferences and workflow needs.
+
+## Features
+
+- Customize Jupyter Notebook interface and appearance.
+- Configure default settings for code execution and output display.
+- Manage keyboard shortcuts and cell behavior.
+- Export and import Jupyter configuration profiles.
+- Integrate with popular Jupyter extensions and plugins.
+- Easy-to-use API for modifying Jupyter settings programmatically.
+
+## Installation
+
+You can install JupyterSettings using pip:
+
+```bash
+pip install jupyter-settings
+
